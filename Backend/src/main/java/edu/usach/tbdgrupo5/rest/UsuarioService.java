9d057b1218fd3ca6436137a636557512c657f390
sebapintoag <sebastian.pintoaguero@usach.cl>
2017-11-27 19:39:27 -0300

    Cors a usuario agregado

--- Backend/src/main/java/edu/usach/tbdgrupo5/rest/UsuarioService.java
+++ Backend/src/main/java/edu/usach/tbdgrupo5/rest/UsuarioService.java
@@ -2,17 +2,12 @@ package edu.usach.tbdgrupo5.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import edu.usach.tbdgrupo5.entities.Usuario;
 import edu.usach.tbdgrupo5.repository.UsuarioRepository;
 
+@CrossOrigin(origins = "http://localhost:8085")
 @RestController  
 @RequestMapping("/usuarios")
 public class UsuarioService {
