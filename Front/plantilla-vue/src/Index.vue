f36b87a3b6acfea422be64a1a2f0a95844f120fb
fermunozO <fernanda.munoz.o@usach.cl>
2017-11-27 21:20:37 -0300

    graficos artistas y generos listos, falta tiempo de estos

--- Front/plantilla-vue/src/Index.vue
+++ Front/plantilla-vue/src/Index.vue
@@ -10,21 +10,20 @@
     <img src="./img/concierto.jpeg" alt="Jeans" style="width:100%">
     <div class="w3-display-topleft w3-text-white" style="padding:24px 48px">
       <h1 class="w3-jumbo w3-hide-small w3-opacity">MusicTweet</h1>
-      <h1 class="w3-jumbo w3-hide-small w3-hover-black">Contamos con X tuits</h1>
     </div>
   </div>
 
 <div class="center">
 
     <div class="w3-card-4 w3-display-left w3-hover-grayscale" style="margin-left: 40px">
-      <a href="#/artistaPositivo"><img class="w3-image" src="./img/sing-hi.png" alt="Norway" style="width:100%"></a>
+      <a href="#/grafArtist"><img class="w3-image" src="./img/sing-hi.png" alt="Norway" style="width:100%"></a>
       <div class="w3-container w3-center w3-black">
         <p>Estadisticas sobre artistas</p>
       </div>
     </div>
 
     <div class="w3-card-4 w3-display-middle w3-hover-grayscale">
-      <a href="#/generoPositivo"><img class="w3-image" src="./img/genero.png" alt="Norway" style="width:100%"></a>
+      <a href="#/grafGen"><img class="w3-image" src="./img/genero.png" alt="Norway" style="width:100%"></a>
       <div class="w3-container w3-center w3-black">
         <p>Estadisticas sobre generos musicales</p>
       </div>
